6f9ac11a9daf4d5aebcc989362052122bf2b94c5
Valery Buchinsky <valery.buchinsky@gmail.com>
2026-01-17 22:38:07 +0200

    Update no-notifications to be a crossed over bell icon

--- web/components/icons/NoNotificationsIcon.tsx
+++ web/components/icons/NoNotificationsIcon.tsx
@@ -19,7 +19,10 @@ export default function NoNotificationsIcon({
       stroke="currentColor"
       {...props}
     >
-      <path d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+      {/* Bell icon path */}
+      <path d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+      {/* Diagonal strike line */}
+      <path d="m3 3 18 18" />
     </svg>
   );
 }
