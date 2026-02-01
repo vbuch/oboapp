b5d64f5913b6ca829cf16198a2179c255264c20a
Valery Buchinsky <valery.buchinsky@gmail.com>
2026-02-01 21:58:31 +0200

    remove unused style

--- web/components/HomeContent.tsx
+++ web/components/HomeContent.tsx
@@ -198,8 +198,8 @@ export default function HomeContent() {
       </div>
 
       {/* Events Sidebar - Right side on desktop, bottom on mobile */}
-      <div className="flex-1 [@media(min-width:1280px)_and_(min-aspect-ratio:4/3)]:w-2/5 bg-white">
-        <div className="p-6 h-full overflow-y-auto @container">
+      <div className="flex-1 [@media(min-width:1280px)_and_(min-aspect-ratio:4/3)]:w-2/5 bg-white overflow-y-auto">
+        <div className="p-6 @container">
           <MessagesGrid
             messages={filteredMessages}
             isLoading={isLoading}
