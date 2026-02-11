3330a841bcc58db6eccf3ce46f844ac7d4346c9a
Valery Buchinsky <valery.buchinsky@gmail.com>
2026-02-11 23:43:46 +0200

    Apply suggestion from @vbuch

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -22,6 +22,8 @@ export const metadata: Metadata = {
     apple: "/icon-192x192.png",
     other: [
       { rel: "icon", url: "/icon-32x32.png", sizes: "32x32" },
+      { rel: "icon", url: "/icon-72x72.png", sizes: "72x72" },
+      { rel: "icon", url: "/icon-192x192.png", sizes: "192x192" },
       { rel: "icon", url: "/icon-512x512.png", sizes: "512x512" },
     ],
   },
